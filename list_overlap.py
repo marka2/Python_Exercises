c52c3958712a6b46cc8341487eabf7db0726cbff
Mark Arakaki <marka2@hawaii.edu>
2017-10-15 02:07:59 -1000

    Added the concept of what program is supposed to do

---
+++ list_overlap.py
@@ -0,0 +1,20 @@
+"""
+
+***********************************
+Author: Mark Arakaki
+October 15, 2017
+Personal Practice Use
+**********************************
+
+Take two lists, say for example these two:
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure the program works on two lists of different sizes.
+
+Extras:
+	1) Randomly generate two lists to test this
+	2) Write this in one line of Python
+
+"""
